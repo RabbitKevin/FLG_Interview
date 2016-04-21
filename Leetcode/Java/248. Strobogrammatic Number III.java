aed4e30e8b8131505a15d67689f1c39478855aaf
Kevin Yang <ykwwind@gmail.com>
2016-04-21 01:28:50 -0700

    leetcode 248 solution 2

--- Leetcode/Java/248. Strobogrammatic Number III.java
+++ Leetcode/Java/248. Strobogrammatic Number III.java
@@ -44,3 +44,37 @@ public class Solution {
         sb.setCharAt(sb.length()-1, x);
     }
 }
+/*
+    从思路1的解法出发，里面可以优化的部分很多.
+    因为不需要去求String， 所以不需要Stringbuilder, 直接换成char[].
+    递归的部分做减枝，把不符合数字形式的递归剪除.
+*/
+public class Solution {
+    char[][] pairs = {{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
+    int count = 0;
+    int count = 0;
+
+    public int strobogrammaticInRange(String low, String high) {
+        for(int len = low.length(); len <= high.length(); len++) {
+            dfs(low, high, new char[len], 0, len - 1);
+        }
+        return count;
+    }
+
+    public void dfs(String low, String high, char[] c, int left, int right) {
+        if(left > right) {
+            String s = new String(c);
+            if((s.length() == low.length() && s.compareTo(low) < 0) ||
+               (s.length() == high.length() && s.compareTo(high) > 0)) return;
+            count++;
+            return;
+        }
+
+        for(char[] p : pairs) {
+            c[left] = p[0];
+            c[right] = p[1];
+            if(c.length != 1 && c[0] == '0') continue;
+            if(left < right || left == right && p[0] == p[1]) dfs(low, high, c, left + 1, right - 1);
+        }
+    }
+}
