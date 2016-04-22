240ce4fe2eee100e9339559dc8f2b4cb86f5b561
Kevin Yang <ykwwind@gmail.com>
2016-04-22 02:58:21 -0700

    divide linkedlist based on fibonacci

---
+++ Amazon/Node.java
@@ -0,0 +1,8 @@
+class Node {
+    int val;
+    Node next;
+    public Node(int num) {
+        val = num;
+        next = null;
+    }
+}
---
+++ Amazon/divide_fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static Node[] divideNumber(List<Integer> nums) {
+
+    }
+    /*
+        http://www.geeksforgeeks.org/check-number-fibonacci-number/
+        不知道链接的话，可以考虑先求最大值，然后做一个fib-dic备查找
+    */
+    private static isFib(int val) {
+
+    }
+}
