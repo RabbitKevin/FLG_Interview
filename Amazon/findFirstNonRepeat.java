0512a8c88b8625f1288f0289cc7043ada7eacd77
Kevin Yang <ykwwind@gmail.com>
2016-04-23 00:41:52 -0700

    Leetcode 345

---
+++ Amazon/findFirstNonRepeat.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void main(String[] args) {
+        List<Character> input = new ArrayList(Arrays.asList('1','2','3','4','8','9','10','2','4','8','9','11','1'));
+    }
+    public static void findFirst(List<Character> input) {
+        Queue<Character> queue = new LinkedList<Character>();
+        
+    }
+}
