ad6a55dca825e2e74ff99f867208a8eba0ff4420
Kevin Yang <ykwwind@gmail.com>
2016-04-29 22:53:04 -0700

    isSubTree

---
+++ G/isSubTree.java
@@ -0,0 +1,21 @@
+/*
+    Determine whether one tree is a subtree of another tree
+    The subtree here is not a part of another tree, it may be the copy of part of that.
+    Can not just compare the TreeNode variable
+*/
+class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isSubTree(TreeNode root, TreeNode node) {
+        if(node == null) return true;
+        if(root == null) return false;
+        if(isIdentical(root, node)) return true;
+        return isSubTree(root.left, node) || isSubTree(root.right, node);
+    }
+    private static boolean isIdentical(TreeNode root, TreeNode node) {
+        if(root == null && node == null) return true;
+        if(root == null || node == null) return false;
+        return root.val == node.val && isIdentical(root.left, node.left) && isIdentical(root.right, node.right)；
+    }
+}
