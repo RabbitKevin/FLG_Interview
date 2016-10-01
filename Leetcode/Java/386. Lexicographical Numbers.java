48c4dbb40d82605049159c53eea78f72b5925892
Kevin Yang <ykwwind@gmail.com>
2016-10-01 13:30:27 -0700

    LC 386

---
+++ Leetcode/Java/386. Lexicographical Numbers.java
@@ -0,0 +1,23 @@
+/*
+    not the fastest
+    recursion, can change to iteration
+
+    Create a help method to get all number starts with number i
+*/
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new LinkedList<>();
+        for(int i = 1; i <= 9; ++i) {
+            help(n, i, result);
+        }
+        return result;
+    }
+    private void help(int n, int i, List<Integer> list) {
+        if(i > n) return;
+        list.add(i);
+        int num = i*10;
+        for(int j = num; j <= Math.min(num+9, n); ++j) {
+            help(n, j, list);
+        }
+    }
+}
